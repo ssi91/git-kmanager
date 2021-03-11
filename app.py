a099fb520ea2cf89f84ee61f62f675467333df19
ssi91 <sergeev.s.i1991@gmail.com>
2021-03-11 16:27:58 +0700

    use fixed git_cli && use requirements.txt

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ clone_path = os.environ.get('CLONE_PATH')
 
 
 def _repo_form_origin(origin: str):
+    # TODO: test that!
     return origin[origin.rfind('/'):origin.rfind('.')]
 
 
@@ -24,14 +25,13 @@ def create_clone():
     data = request.json
     origin_repository = data['origin']
     repository_name = data['repository'] or _repo_form_origin(origin_repository)
-    org_name = data['org_name']  # TODO: fill the org's name
-    # with GitCLIClient(config.get('ssh-key-path'), '/tmp') as git_cli:  # FIXME: context manager is not working
-    git_cli = GitCLIClient(config.get('ssh-key-path'), clone_path)
-    git_cli.clone(origin_repository)
-    gh_client = GitHubOrgAPIClient(org_name, config.get('credentials'))
-    response = gh_client.create_repository(name=repository_name)
-    ssh_url = response['ssh_url']
-    os.chdir(f'{clone_path}/{repository_name}')
-    git_cli.push(ssh_url, 'master')  # TODO: use actual default branch
+    org_name = data.get('org_name', config.get('org_name'))
+    with GitCLIClient(config.get('ssh-key-path'), clone_path) as git_cli:
+        git_cli.clone(origin_repository)
+        gh_client = GitHubOrgAPIClient(org_name, config.get('credentials'))
+        response = gh_client.create_repository(name=repository_name)
+        ssh_url = response['ssh_url']
+        os.chdir(f'{clone_path}/{repository_name}')
+        git_cli.push(ssh_url, 'master')  # TODO: use actual default branch
 
     return 'success'
